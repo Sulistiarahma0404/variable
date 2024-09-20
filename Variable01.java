e662c2de65fd23b8bce917bb06e0d3a882864705
Sulistiarahma0404 <Sulistiarah@gmail.com>
2024-09-19 20:26:44 -1200

    Add files via upload

---
+++ Variable01.java
@@ -0,0 +1,21 @@
+public class Variable01 {
+    public static void main(String[] args) {
+        // deklarasi variable
+        String nama ;
+        int umur;
+
+        // input nilai
+        nama = "Sulistia rahma";
+        umur = 18;
+
+        // output ke layar
+        System.out.println("Nama: " + nama );
+        System.out.println("Umur: " + umur );
+
+        nama = "Ahmad Junaidi";
+        System.out.println("Pacarku namanya " + nama);
+
+
+    
+    }
+}
